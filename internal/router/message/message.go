614d6d53da7278114b0a54b53654ce73e6113660
egor <burcev-egor@mail.ru>
2024-07-22 15:58:07 +0300

    msg routes

--- internal/router/message/message.go
+++ internal/router/message/message.go
@@ -1,26 +1,57 @@
 package router
 
 import (
-	"test-service/internal/models"
-	"test-service/internal/router"
+	"test-service/internal/converter"
+	"test-service/internal/model"
 
 	"github.com/valyala/fasthttp"
 )
 
 type Database interface {
 	SaveMessage() error
-	GetMessages() ([]models.Message, error)
+	GetMessages() ([]model.Message, error)
 }
 
-func RegisterMessage(r *router.Router) {
-	r.r.POST("message", receiveMessage)
-	r.r.GET("/messages/stats", getStatistics)
+type messageImpl struct {
+	r *Router
 }
 
-func receiveMessage(ctx *fasthttp.RequestCtx) {
-
+func RegisterMessage(r *Router) {
+	msgImpl := messageImpl{r}
+	r.router.POST("message", msgImpl.receiveMessage)
+	r.router.GET("/messages/stats", msgImpl.getStatistics)
 }
 
-func getStatistics(ctx *fasthttp.RequestCtx) {
+func (m *messageImpl) receiveMessage(ctx *fasthttp.RequestCtx) {
+	data := ctx.Request.Body()
+	msg, err := converter.JsonToMessage(data)
+	if err != nil {
+		m.r.logger.Errorf("could not read input message")
+		ctx.SetStatusCode(500)
+		return
+	}
 
+	err = m.r.msgService.SaveMessage(msg)
+	if err != nil {
+		m.r.logger.Errorf("could not save message: %e", err)
+		ctx.SetStatusCode(500)
+		return
+	}
+}
+
+func (m *messageImpl) getStatistics(ctx *fasthttp.RequestCtx) {
+	unProcessed, processed, err := m.r.msgService.GetStatistics()
+	if err != nil {
+		m.r.logger.Errorf("could not get statistics: %e", err)
+		ctx.SetStatusCode(500)
+		return
+	}
+
+	data, err := converter.StatisticsToJson(unProcessed, processed)
+	if err != nil {
+		m.r.logger.Errorf("could not create response body: %e", err)
+		ctx.SetStatusCode(500)
+		return
+	}
+	ctx.Response.AppendBody(data)
 }
--- internal/router/message/router.go
+++ internal/router/message/router.go
@@ -1,21 +1,24 @@
 package router
 
 import (
+	"test-service/internal/service"
 
 	"github.com/fasthttp/router"
 	"github.com/sirupsen/logrus"
 )
 
 type Router struct {
-	r *router.Router
-	l *logrus.Logger
+	router     *router.Router
+	logger     *logrus.Logger
+	msgService service.MessageService
 }
 
-func NewRouter(logger *logrus.Logger) *Router {
-	r := router.New()
-	RegisterMessage(r)
-	return &Router{
-		r: r,
-		l: logger,
+func NewRouter(logger *logrus.Logger, msgService *service.MessageService) *Router {
+	r := &Router{
+		router:     router.New(),
+		logger:     logger,
+		msgService: *msgService,
 	}
+	RegisterMessage(r)
+	return r
 }
