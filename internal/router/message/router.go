d52c0c63de8da6dec9ec109f5facaa115f9920d9
beus <burcev-egor@mail.ru>
2024-07-24 10:56:06 +0300

    router fix

--- internal/router/message/router.go
+++ internal/router/message/router.go
@@ -18,14 +18,18 @@ type Router struct {
 }
 
 func NewRouter(logger *logrus.Logger, msgService service.MessageService, cfg config.Config) *Router {
+	router := router.New()
 	r := &Router{
-		router:     router.New(),
+		router:     router,
 		logger:     logger,
 		msgService: msgService,
 		port:       cfg.Service.Port,
-		srv:        &fasthttp.Server{},
+		srv: &fasthttp.Server{
+			Handler: router.Handler,
+		},
 	}
 	RegisterMessage(r)
+	r.router.GET("/status", statusHandler)
 	return r
 }
 
@@ -35,4 +39,8 @@ func (r *Router) Start() error {
 
 func (r *Router) Shutdown() error {
 	return r.srv.Shutdown()
+}
+
+func statusHandler(ctx *fasthttp.RequestCtx) {
+	ctx.SetStatusCode(fasthttp.StatusOK)
 }
