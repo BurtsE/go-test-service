2eb6aa0f3f9a8bd2759a735675c488f0a263e6c5
beus <burcev-egor@mail.ru>
2024-07-24 10:43:54 +0300

    app start

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"context"
+	"log"
+	"test-service/internal/app"
+)
+
 func main() {
-	
+	ctx := context.Background()
+
+	a := app.NewApp(ctx)
+	if a == nil {
+		log.Fatalf("failed to create app")
+	}
+	err := a.Run()
+	if err != nil {
+		log.Fatalf("failed to run app: %s", err.Error())
+	}
 }
---
+++ internal/app/service_provider.go
@@ -0,0 +1,58 @@
+package app
+
+import (
+	"log"
+	"test-service/internal/config"
+	repo "test-service/internal/repo/message"
+	"test-service/internal/repo/message/postgres"
+	router "test-service/internal/router/message"
+	"test-service/internal/service"
+	"test-service/internal/service/message"
+
+	"github.com/sirupsen/logrus"
+)
+
+type serviceProvider struct {
+	cfg               *config.Config
+	messageRepository repo.MessageRepository
+	messageService    service.MessageService
+	router            *router.Router
+}
+
+func NewSericeProvider() *serviceProvider {
+	s := &serviceProvider{}
+	s.Router()
+	return s
+}
+
+func (s *serviceProvider) Config() *config.Config {
+	if s.cfg == nil {
+		cfg, err := config.InitConfig()
+		if err != nil {
+			log.Fatal(err)
+		}
+		s.cfg = cfg
+	}
+	return s.cfg
+}
+
+func (s *serviceProvider) MessageRepository() repo.MessageRepository {
+	if s.messageRepository == nil {
+		s.messageRepository = postgres.NewRepository(nil, nil)
+	}
+	return s.messageRepository
+}
+
+func (s *serviceProvider) MessageService() service.MessageService {
+	if s.messageService == nil {
+		s.messageService = message.NewService(s.MessageRepository(), s.cfg)
+	}
+	return s.messageService
+}
+
+func (s *serviceProvider) Router() *router.Router {
+	if s.router == nil {
+		s.router = router.NewRouter(logrus.New(), s.MessageService(), *s.Config())
+	}
+	return s.router
+}
