1e8eb1c28bba4fc7ca87a70da93ddd2564f19d01
beus <burcev-egor@mail.ru>
2024-07-24 11:07:23 +0300

    sslmode

--- internal/config/config.go
+++ internal/config/config.go
@@ -14,6 +14,7 @@ type Postgres struct {
 	Port     string `json:"port"`
 	DB       string `json:"db"`
 	MaxConns int    `json:"max_conns"`
+	Sslmode  string `json:"sslmode"`
 	User     string `env:"PG_USER,notEmpty"`
 	Password string `env:"PG_PASSWORD,notEmpty"`
 }
